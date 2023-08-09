51d336d4e3a87df3f7e5e9f7b24957e9214b3dae
Sariolka <sariola@list.ru>
2023-08-09 17:27:11 +0300

    limiter

--- app.js
+++ app.js
@@ -3,22 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const cors = require('./middlewares/cors');
+const limiter = require('./middlewares/rateLimiter');
 const router = require('./routes');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
 app.use(express.json());
+app.use(limiter);
 app.use(cors);
 
 app.use(requestLogger);
