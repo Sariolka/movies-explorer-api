bc9560ac282e4e85d84fb88c19b8d126af58feb2
Роза <119030348+Sariolka@users.noreply.github.com>
2023-09-07 16:24:16 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -42,7 +42,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.status(OK).send({ user.name, user.email, token });
+      res.status(OK).send({ email: user.email, name: user.name, token });
     })
     .catch(() => {
       throw new UnauthorizedError('Необходима авторизация');
