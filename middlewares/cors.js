cd19ff82d2148b9d3c8a273d47769fc7de955e04
Роза <119030348+Sariolka@users.noreply.github.com>
2023-09-10 19:31:55 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,1 @@
-module.exports = (req, res, next) => {
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  res.header('Access-Control-Allow-Origin', '*');
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  return next();
-};
