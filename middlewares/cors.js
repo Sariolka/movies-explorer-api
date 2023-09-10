e66c04f0a1bfc74c1f5c5fdbfbfc729c5841916a
Роза <119030348+Sariolka@users.noreply.github.com>
2023-09-10 19:26:22 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,32 +1,15 @@
-const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'https://api.sariola.diploma.nomoreparties.co',
-  'http://api.sariola.diploma.nomoreparties.co',
-  'https://sariola.diploma.nomoreparties.co',
-  'http://sariola.diploma.nomoreparties.co',
-];
-
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
+module.exports = (req, res, next) => {
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    res.end();
+    return res.end();
   }
 
-  next();
+  return next();
 };
-
-module.exports = cors;
